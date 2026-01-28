1b2aaeaf7b233b67c4a21ec5aff2540e67e090b9
Ahsanul Kabir <kowshid@gmail.com>
2026-01-28 15:43:34 -0500

    CW03: Process The Data

---
+++ Classworks/dvc-exercise/src/make_processed.py
@@ -0,0 +1,31 @@
+# cleans and preprocesses the raw data. I want only take out the following
+# flower rows: Setosas for which Petal width is less than 0.2, Versicolor for which Sepal width
+# is more than 3 and Virginica for which sepal length is more than 6.5. Make sure these rules
+# are not magic numbers in your code.
+# "sepal.length","sepal.width","petal.length","petal.width","variety"
+
+
+import pandas as pd
+import yaml
+from pathlib import Path
+
+RAW = Path("data/raw/iris.csv")
+OUT = Path("data/processed/iris_cleaned.csv")
+PARAMS = Path("params.yml")
+params = yaml.safe_load(PARAMS.read_text())
+
+max_petal_width = params["cleaning"]["max_petal_width"]
+min_sepal_width = params["cleaning"]["min_sepal_width"]
+min_sepal_length = params["cleaning"]["min_sepal_length"]
+
+df = pd.read_csv(RAW)
+
+target_setosa = (df["variety"] == "Setosa") & (df["petal.width"] < max_petal_width)
+target_versicolor = (df["variety"] == "Versicolor") & (df["sepal.width"] > min_sepal_width)
+target_virginica = (df["variety"] == "Virginica") & (df["sepal.length"] > min_sepal_length)
+
+final_df = df[target_setosa | target_versicolor | target_virginica]
+
+OUT.parent.mkdir(parents=True, exist_ok=True)
+final_df.to_csv(OUT, index=False)
+print(f"Wrote: {OUT}")
